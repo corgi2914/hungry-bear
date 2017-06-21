f3cae42cd091f2c0b41ab6bdd71b8c2b31a7edb0
corgi2914 <fluff29@icloud.com>
2017-06-20 14:57:43 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function eatHumans(bearName, humanNames){
+	console.log(bearName);
+	console.log(humanNames);
+}
+
+eatHumans("Christie", ["Bob", "Jill", "Bill"]);
+console.log(humanNames[0] + "was eaten by a" bearName);
+console.log(humanNames[1] + "was eaten by a" bearName);
+console.log(humanNames[2] + "was eaten by a" bearName); 
